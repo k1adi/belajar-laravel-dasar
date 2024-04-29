02eff3fae2db1972e445d7e7055727cfb29f1752
k1adi <rizkiadinugroho3@gmail.com>
2024-04-29 09:31:04 +0700

    added method singleton

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -15,6 +15,7 @@ class ServiceContainerTest extends TestCase
      */
     public function test_example()
     {
+        // Dependency injection with service container
         $foo1 = $this->app->make(Foo::class);
         $foo2 = $this->app->make(Foo::class);
 
@@ -28,15 +29,36 @@ class ServiceContainerTest extends TestCase
         // $person = $this->app->make(Person::class);
         // self::assertNotNull($person);
 
+        // method bind(key, closure)
+        // key = Object / class
+        // closure = Nested function (declration function inside function)
         $this->app->bind(Person::class, function($app) {
-            return new Person("Rizki", "Adi");
+            return new Person("Rizki", "Adi"); // Create new object
         });
 
-        $person1 = $this->app->make(Person::class);
-        $person2 = $this->app->make(Person::class);
+        // Ketika menjalankan make maka closure akan otomatis di panggil
+        $person1 = $this->app->make(Person::class); //closure() -> new Person("Rizki", "Adi");
+        $person2 = $this->app->make(Person::class); //closure() -> new Person("Rizki", "Adi");
 
         self::assertEquals("Rizki", $person1->firstname);
         self::assertEquals("Rizki", $person2->firstname);
         self::assertNotSame($person1, $person2);
+    }
+
+    public function test_singleton()
+    {
+        // method singleton(bind, closure)
+        // cara membuat object baru beserta cara pembuatannya
+        $this->app->singleton(Person::class, function($app) {
+            return new Person("Rizki", "Adi"); // Create one object
+        });
+
+        // Ketika menjalankan make maka closure akan otomatis di panggil
+        $person1 = $this->app->make(Person::class); // new Person("Rizki", "Adi"); if not exists
+        $person2 = $this->app->make(Person::class); // return existing object
+
+        self::assertEquals("Rizki", $person1->firstname);
+        self::assertEquals("Rizki", $person2->firstname);
+        self::assertSame($person1, $person2);
     }
 }
